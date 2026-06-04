a05445a14dd34761537bdcc6cd50c8ea667b6dd8
Victor <136484986+Oisasoje@users.noreply.github.com>
2026-06-04 01:18:55 +0100

    Seaparate auth attempts in DB and create signup endpoint

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-if (process.env.NODE_ENV !== "production") {
-    app.listen(3000, () => console.log("Server running on port 3000"));
-}
-// Export for Vercel
-export default app;
