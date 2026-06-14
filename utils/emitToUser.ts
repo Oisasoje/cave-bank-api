cb170c2ca7f8be544e3beb6438fe6b8a9def0ac3
Victor <136484986+Oisasoje@users.noreply.github.com>
2026-06-14 17:01:08 +0100

    Correct io path

--- utils/emitToUser.ts
+++ utils/emitToUser.ts
@@ -1,4 +1,4 @@
-import { io } from "../realTime/socket.js";
+import { io } from "../app.js";
 
 export const emitToUser = (userId: string, event: string, payload: any) => {
   io.to(`user:${userId}`).emit(event, payload);
